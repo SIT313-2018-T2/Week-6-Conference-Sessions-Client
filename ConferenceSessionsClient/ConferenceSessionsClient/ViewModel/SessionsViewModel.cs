85f3ce3998541bc9a8b81bf3dcc80ebfe93fe8b7
darkeung <hawygfau@gmail.com>
2018-09-10 13:22:09 +1000

    wk6

---
+++ ConferenceSessionsClient/ConferenceSessionsClient/ViewModel/SessionsViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using ConferenceSessionsClient.Data;
+using ConferenceSessionsClient.Model;
+
+namespace ConferenceSessionsClient.ViewModel
+{
+    public class SessionsViewModel : INotifyPropertyChanged
+    {
+        SessionsManager sessionsManager = new SessionsManager();
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ObservableCollection<Session> _sessionslist;
+        public ObservableCollection<Session> SessionsList
+        {
+            get { return _sessionslist; }
+            set
+            {
+                _sessionslist = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("SessionsList"));
+                }
+            }
+        }
+        public async Task FetchDataAsync()
+        {
+            var list = await sessionsManager.FetchSessionsAsync();
+            SessionsList = new ObservableCollection<Session>(list);
+        }
+        public SessionsViewModel()
+        {
+            FetchDataAsync();
+        }
+    }
+}
